aefee1f7516a78d2c8239a536c69dfc45f8b9f56
Devaneil <51035563+devaneil@users.noreply.github.com>
2021-09-27 20:23:23 +1000

    Add files via upload

---
+++ Crop Recommender/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from pickle import load
+import numpy as np
+
+app = Flask(__name__) # Instantiating Flask class.
+
+model = load(open('ensemble.pkl', 'rb')) # Loading the trained model for prediction.
+label = load(open('label.pkl', 'rb')) # Loading the label encoder to inverse transform the predicted class by the model.
+
+@app.route('/')
+def main():
+    return render_template('home.html')
+
+@app.route('/predict', methods = ['POST'])
+def home():
+    # Retrieving the data entered by the user.
+    nitrogen = request.form['N']
+    potassium = request.form['K']
+    temperature = request.form['Temp']
+    humidity = request.form['Humid']
+    pH = request.form['pH']
+    rainfall = request.form['Rain']
+    # Converting the values entered by the user into NumPy array to feed the model.
+    data = np.array([[nitrogen, potassium, temperature, humidity, pH, rainfall]]) 
+    predicted = model.predict(data) # Prediction.
+    predicted_class = label.inverse_transform(predicted)[0] # Inverse transforming the predicted class.
+    return render_template('result.html', data = predicted_class)
+
+if __name__ == '__main__':
+    app.run(debug = True)
+    
